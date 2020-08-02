95132c3eefc50cafc8f7cd985d317b4c07971a68
Joran Dob <joran998@gmail.com>
2020-08-02 22:07:15 +0200

    Fixed import issue.

--- test/flutter_subtitle_wrapper_srt_test.dart
+++ test/flutter_subtitle_wrapper_srt_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:subtitle_wrapper_package/data/models/subtitle.dart';
 import 'package:subtitle_wrapper_package/data/models/subtitles.dart';
