405e79850f7f5415a80fda24e9ab4b79c35d9f98
Lukasz Wisniewski <luke.cherrish@gmail.com>
2021-11-22 16:27:27 +0100

    update to bloc v8
    closes #62

--- lib/bloc/subtitle/subtitle_bloc.dart
+++ lib/bloc/subtitle/subtitle_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:subtitle_wrapper_package/data/models/subtitle.dart';
 import 'package:subtitle_wrapper_package/data/models/subtitles.dart';
@@ -23,30 +24,28 @@ class SubtitleBloc extends Bloc<SubtitleEvent, SubtitleState> {
     required this.subtitleRepository,
     required this.subtitleController,
   }) : super(SubtitleInitial()) {
+    on<SubtitleEvent>(_onEvent, transformer: sequential());
     subtitleController.attach(this);
   }
 
-  @override
-  Stream<SubtitleState> mapEventToState(
-    SubtitleEvent event,
-  ) async* {
+  FutureOr<void> _onEvent(SubtitleEvent event, Emitter emit) async {
     if (event is LoadSubtitle) {
-      yield* loadSubtitle();
+      return _loadSubtitle(event, emit);
     } else if (event is InitSubtitles) {
-      yield* initSubtitles();
+      return _initSubtitles(event, emit);
     } else if (event is UpdateLoadedSubtitle) {
-      yield LoadedSubtitle(event.subtitle);
+      emit(LoadedSubtitle(event.subtitle));
     }
   }
 
-  Stream<SubtitleState> initSubtitles() async* {
-    yield SubtitleInitializating();
+  FutureOr<void> _initSubtitles(InitSubtitles event, Emitter emit) async {
+    emit(SubtitleInitializating());
     subtitles = await subtitleRepository.getSubtitles();
-    yield SubtitleInitialized();
+    emit(SubtitleInitialized());
   }
 
-  Stream<SubtitleState> loadSubtitle() async* {
-    yield LoadingSubtitle();
+  FutureOr<void> _loadSubtitle(LoadSubtitle event, Emitter emit) async {
+    emit(LoadingSubtitle());
     videoPlayerController.addListener(
       () {
         final videoPlayerPosition = videoPlayerController.value.position;
