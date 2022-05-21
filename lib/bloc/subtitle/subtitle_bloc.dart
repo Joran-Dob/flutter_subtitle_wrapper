6b498a6f2c2980248d4fd8e3e0e3728c4bc769e0
Joran Dob <joran998@gmail.com>
2022-05-21 21:11:06 +0200

    Reformatted code.

--- lib/bloc/subtitle/subtitle_bloc.dart
+++ lib/bloc/subtitle/subtitle_bloc.dart
@@ -47,10 +47,10 @@ class SubtitleBloc extends Bloc<SubtitleEvent, SubtitleState> {
       () {
         final videoPlayerPosition = videoPlayerController.value.position;
         for (final Subtitle subtitleItem in subtitles.subtitles) {
-          final bool validStartTime =
-              videoPlayerPosition.inMilliseconds > subtitleItem.startTime.inMilliseconds;
-          final bool validEndTime =
-              videoPlayerPosition.inMilliseconds < subtitleItem.endTime.inMilliseconds;
+          final bool validStartTime = videoPlayerPosition.inMilliseconds >
+              subtitleItem.startTime.inMilliseconds;
+          final bool validEndTime = videoPlayerPosition.inMilliseconds <
+              subtitleItem.endTime.inMilliseconds;
           if (validStartTime && validEndTime) {
             add(
               UpdateLoadedSubtitle(
